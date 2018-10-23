06ca6175c564e20292cf480fdd692fc02d188245
Diogo Ide <diogoide@gmail.com>
2018-10-23 23:41:37 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,9 +6,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_employees
+      t.integer :num_of_employees
       t.boolean :still_in_business
-      t.string :opening_time
+      t.string :start_tim
       t.string :closing_time
     end
   end
